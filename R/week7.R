fd9cac5e87b5ac4141b9c4c9afa1ec482d4170c8
Saron M Demeke <demek004@umn.edu>
2023-03-01 19:27:25 -0600

    working with fig 1 and 2

--- R/week7.R
+++ R/week7.R
@@ -13,3 +13,22 @@ week7_tbl <- read_csv("../data/week3.csv") %>%
   filter(q6==1) %>% 
   select(-c(q6)) %>% 
   mutate(timeSpent = timeEnd - timeStart)
+
+
+
+
+
+#Visualization
+week7_tbl %>% 
+  GGally::ggpairs(columns = 5:13) #non index way to reference cols?
+
+p1 <- ggplot(data=week7_tbl,aes(x=timeStart,y=q1)) +
+  geom_point() +
+  labs(x="Date of Experiment",y="Q1 Score") 
+ggsave(filename ="../figs/fig1.png",plot=p1) #pipe within generation
+
+
+p2 <- ggplot(data=week7_tbl,aes(x=q1,y=q2,color=gender)) +
+  geom_jitter() +
+  labs(color = "Participant Gender")
+ggsave(filename ="../figs/fig2.png",plot=p2) #sizing not great, legend half too big
